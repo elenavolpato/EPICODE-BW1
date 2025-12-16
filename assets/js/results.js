da725785b4f175df529f06ef9ea52861dbe062ea
DattolaFrancesco <dattolafrancescoo@gmail.com>
2025-12-16 16:52:33 +0100

    cake added, h1 cheanged with h2

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,17 @@
 const canvas = document.querySelector("#grafico");
 
-new Chart();
+new Chart(canvas, {
+  type: "doughnut",
+  data: {
+    datasets: [
+      {
+        data: [30, 70],
+        backgroundColor: ["#C2128D", "#00FFFF"],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    cutout: "72%",
+  },
+});
