6ade0ffa81f849cdd1d65905da4153e85bbb51d5
DattolaFrancesco <dattolafrancescoo@gmail.com>
2025-12-17 20:20:45 +0100

    result js

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,11 +1,23 @@
+let trueAnswers = localStorage.getItem("answersTrue");
+let falseAnswers = localStorage.getItem("answersFalse");
+let percentualiL = (trueAnswers / 5) * 100;
+let percentualiR = (falseAnswers / 5) * 100;
 const canvas = document.querySelector("#grafico");
+const numLeft = document.querySelector("#percentageNumberLeft");
+const numRight = document.querySelector("#percentageNumberRight");
+const spanL = document.querySelector("#spanL");
+const spanR = document.querySelector("#spanR");
+numLeft.innerText = percentualiL + "%";
+numRight.innerText = percentualiR + "%";
+spanL.innerText = trueAnswers;
+spanR.innerText = falseAnswers;
 
 new Chart(canvas, {
   type: "doughnut",
   data: {
     datasets: [
       {
-        data: [30, 70],
+        data: [falseAnswers, trueAnswers],
         backgroundColor: ["#C2128D", "#00FFFF"],
         borderWidth: 0,
       },
