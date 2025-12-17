d3f8c8ba7b6dfea89540c392a0aa598ee2766441
DattolaFrancesco <dattolafrancescoo@gmail.com>
2025-12-17 10:13:31 +0100

    test

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,37 +1,94 @@
-let timeLeft = 60;
-let timerElement = document.getElementById("time");
-let answers = document.querySelectorAll(".answer");
+const numberCountDown = document.querySelector("#numero");
+const question = document.querySelector("#question");
+const answers = document.querySelector("#answers");
+const answer1 = document.querySelector("#answer1");
+const answer2 = document.querySelector("#answer2");
+const answer3 = document.querySelector("#answer3");
+const answer4 = document.querySelector("#answer4");
+const numerator = document.querySelector("#numerator");
+let counter = 0;
+let n = 0;
+let answerTrue = [];
+let answerFalse = [];
 
-const timer = setInterval(function () {
-  timeLeft--;
-  timerElement.innerText = timeLeft;
-
-  if (timeLeft === 0) {
-    clearInterval(timer);
-    disableAnswers();
+const timer = setInterval(() => {
+  if (parseInt(numberCountDown.innerText) > 0) {
+    numberCountDown.innerText -= 1;
+  } else {
+    counter++;
+    numerator.innerText = counter;
+    numberCountDown.innerText = 30;
+    numerator;
+    if (counter >= 5) window.location.href = "result.html";
+    Question();
   }
 }, 1000);
 
-answers.forEach(function (button) {
-  button.addEventListener("click", function () {
-    clearInterval(timer);
+// oggetti per le domande
 
-    if (button.dataset.correct === "true") {
-      button.classList.add("correct");
-    } else {
-      button.classList.add("wrong");
-      answers.forEach(function (btn) {
-        if (btn.dataset.correct === "true") {
-          btn.classList.add("correct");
-        }
-      });
-    }
+const Questions = [
+  {
+    question: "come si chiama paola?",
+    answer: ["pimpi", "pompi", "pumpi", "pimpo"],
+    correctAnswer: 0,
+  },
+  {
+    question: "come si chiama fra?",
+    answer: ["an", "anakin", "pufd", "fdmpo"],
+    correctAnswer: 1,
+  },
+  {
+    question: "come si chiama gio?",
+    answer: ["gio", "anakin", "giovanni", "fdmpo"],
+    correctAnswer: 2,
+  },
+  {
+    question: "come si chiama mamma?",
+    answer: ["an", "anakin", "pufd", "fcate"],
+    correctAnswer: 3,
+  },
+  {
+    question: "come si chiama papa?",
+    answer: ["an", "umberto", "pufd", "fcate"],
+    correctAnswer: 1,
+  },
+];
 
-    disableAnswers();
-  });
-});
-function disableAnswers() {
-  answers.forEach(function (button) {
-    button.disabled = true;
-  });
-}
+const Question = () => {
+  question.innerText = Questions[counter].question;
+  answer1.innerText = Questions[counter].answer[0];
+  answer2.innerText = Questions[counter].answer[1];
+  answer3.innerText = Questions[counter].answer[2];
+  answer4.innerText = Questions[counter].answer[3];
+};
+
+answers.onclick = (e) => {
+  switch (counter) {
+    case 0:
+      if (e.target.id === "answer1") answerTrue.push("*");
+      else answerFalse.push("*");
+      break;
+    case 1:
+      if (e.target.id === "answer2") answerTrue.push("*");
+      else answerFalse.push("*");
+      break;
+    case 2:
+      if (e.target.id === "answer3") answerTrue.push("*");
+      else answerFalse.push("*");
+      break;
+    case 3:
+      if (e.target.id === "answer4") answerTrue.push("*");
+      else answerFalse.push("*");
+      break;
+    case 4:
+      if (e.target.id === "answer2") answerTrue.push("*");
+      else answerFalse.push("*");
+      break;
+  }
+  counter++;
+  numerator.innerText = counter;
+  numberCountDown.innerText = 30;
+  if (counter < 5) Question();
+  else window.location.href = "result.html";
+};
+Question();
