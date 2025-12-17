d425b6c71193ac4dc743a9eede272b33f84dc8de
DattolaFrancesco <dattolafrancescoo@gmail.com>
2025-12-17 11:49:46 +0100

    adjusted Questions function

--- assets/js/test.js
+++ assets/js/test.js
@@ -5,27 +5,8 @@ const answer1 = document.querySelector("#answer1");
 const answer2 = document.querySelector("#answer2");
 const answer3 = document.querySelector("#answer3");
 const answer4 = document.querySelector("#answer4");
-const numerator = document.querySelector("#numerator");
-let counter = 0;
-let n = 0;
-let answerTrue = [];
-let answerFalse = [];
-
-const timer = setInterval(() => {
-  if (parseInt(numberCountDown.innerText) > 0) {
-    numberCountDown.innerText -= 1;
-  } else {
-    counter++;
-    numerator.innerText = counter;
-    numberCountDown.innerText = 30;
-    numerator;
-    if (counter >= 5) window.location.href = "result.html";
-    Question();
-  }
-}, 1000);
-
-// oggetti per le domande
-
+const risposteSingole = document.querySelectorAll(".answer");
+let arrayAnswer = [answer1, answer2, answer3, answer4];
 const Questions = [
   {
     question: "come si chiama paola?",
@@ -54,41 +35,62 @@ const Questions = [
   },
 ];
 
+risposteSingole.forEach((risposta) => {
+  risposta.onclick = (e) => {
+    switch (counter) {
+      case 0:
+        if (e.target.id === "answer1") answerTrue.push("*");
+        else answerFalse.push("*");
+        break;
+      case 1:
+        if (e.target.id === "answer2") answerTrue.push("*");
+        else answerFalse.push("*");
+        break;
+      case 2:
+        if (e.target.id === "answer3") answerTrue.push("*");
+        else answerFalse.push("*");
+        break;
+      case 3:
+        if (e.target.id === "answer4") answerTrue.push("*");
+        else answerFalse.push("*");
+        break;
+      case 4:
+        if (e.target.id === "answer2") answerTrue.push("*");
+        else answerFalse.push("*");
+        break;
+    }
+    console.log(answerFalse);
+    console.log(answerTrue);
+    counter++;
+    numerator.innerText = counter;
+    numberCountDown.innerText = 30;
+    if (counter < 5) Question();
+    else window.location.href = "results.html";
+  };
+});
+const numerator = document.querySelector("#numerator");
+let counter = 0;
+let n = 0;
+let answerTrue = [];
+let answerFalse = [];
+
+const timer = setInterval(() => {
+  if (parseInt(numberCountDown.innerText) > 0) {
+    numberCountDown.innerText -= 1;
+  } else {
+    counter++;
+    numerator.innerText = counter;
+    numberCountDown.innerText = 30;
+    numerator;
+    if (counter >= 5) window.location.href = "results.html";
+    Question();
+  }
+}, 1000);
+
 const Question = () => {
   question.innerText = Questions[counter].question;
-  answer1.innerText = Questions[counter].answer[0];
-  answer2.innerText = Questions[counter].answer[1];
-  answer3.innerText = Questions[counter].answer[2];
-  answer4.innerText = Questions[counter].answer[3];
-};
-
-answers.onclick = (e) => {
-  switch (counter) {
-    case 0:
-      if (e.target.id === "answer1") answerTrue.push("*");
-      else answerFalse.push("*");
-      break;
-    case 1:
-      if (e.target.id === "answer2") answerTrue.push("*");
-      else answerFalse.push("*");
-      break;
-    case 2:
-      if (e.target.id === "answer3") answerTrue.push("*");
-      else answerFalse.push("*");
-      break;
-    case 3:
-      if (e.target.id === "answer4") answerTrue.push("*");
-      else answerFalse.push("*");
-      break;
-    case 4:
-      if (e.target.id === "answer2") answerTrue.push("*");
-      else answerFalse.push("*");
-      break;
+  for (let i = 0; i < 4; i++) {
+    arrayAnswer[i].innerText = Questions[counter].answer[i];
   }
-  counter++;
-  numerator.innerText = counter;
-  numberCountDown.innerText = 30;
-  if (counter < 5) Question();
-  else window.location.href = "result.html";
 };
 Question();
