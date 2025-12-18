2f487df46753820d509fd2dad60bb6adea5fe775
raposinha <elena@raposinha.dev>
2025-12-18 12:24:45 +0100

    fixes results issue

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,7 +1,11 @@
-let trueAnswers = localStorage.getItem("answersTrue");
-let falseAnswers = localStorage.getItem("answersFalse");
-let percentualiL = (trueAnswers / 5) * 100;
-let percentualiR = (falseAnswers / 5) * 100;
+let trueAnswers = localStorage.getItem("correctAnswers");
+let falseAnswers = localStorage.getItem("wrongAnswers");
+let totalAnswers = parseInt(trueAnswers) + parseInt(falseAnswers);
+
+console.log(totalAnswers);
+let percentualiL = (trueAnswers / totalAnswers) * 100;
+let percentualiR = (falseAnswers / totalAnswers) * 100;
+
 const canvas = document.querySelector("#grafico");
 const numLeft = document.querySelector("#percentageNumberLeft");
 const numRight = document.querySelector("#percentageNumberRight");
@@ -9,8 +13,8 @@ const spanL = document.querySelector("#spanL");
 const spanR = document.querySelector("#spanR");
 numLeft.innerText = percentualiL + "%";
 numRight.innerText = percentualiR + "%";
-spanL.innerText = trueAnswers;
-spanR.innerText = falseAnswers;
+spanL.innerText = trueAnswers + " / " + totalAnswers + " questions";
+spanR.innerText = falseAnswers + " / " + totalAnswers + " questions";
 
 new Chart(canvas, {
   type: "doughnut",
